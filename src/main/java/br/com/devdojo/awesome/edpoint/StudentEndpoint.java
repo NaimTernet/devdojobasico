3ee4c3f87365e61b16e0d0054c2454ecb918e242
Naim Chagas <re040879@qintess.com>
2021-10-29 07:51:24 -0300

    Atualizacao do codigo com as anotacoes Mapping para os metodos GET , PUT DELETE

--- src/main/java/br/com/devdojo/awesome/edpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/awesome/edpoint/StudentEndpoint.java
@@ -3,11 +3,9 @@ package br.com.devdojo.awesome.edpoint;
 import br.com.devdojo.awesome.error.CustomErrorType;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 import br.com.devdojo.awesome.model.Student;
@@ -23,21 +21,42 @@ public class StudentEndpoint {
         this.dateUtil = dateUtil;
 
     }
+
     @RequestMapping(method = RequestMethod.GET)
-    public ResponseEntity <?> ListAll() {
-        return new ResponseEntity<> (Student.studentList, HttpStatus.OK);
+    public ResponseEntity<?> ListAll() {
+        return new ResponseEntity<>(Student.studentList, HttpStatus.OK);
     }
 
     @RequestMapping(method = RequestMethod.GET, path = "/{id}")
     public ResponseEntity<?> getStudentById(@PathVariable("id") int id) {
-    	Student student = new Student();
-    	student.setId(id);
-    	int index = Student.studentList.indexOf(student);
-    	if(index == -1)
-		return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
-		return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
+        Student student = new Student();
+        student.setId(id);
+        int index = Student.studentList.indexOf(student);
+        if (index == -1) return new ResponseEntity<>(new CustomErrorType("Student not found"), HttpStatus.NOT_FOUND);
+        return new ResponseEntity<>(Student.studentList.get(index), HttpStatus.OK);
     }
-}
 
+//    @RequestMapping(method = RequestMethod.POST)
+//    public RequestEntity<?> save(@RequestBody Student student) {
+//        Student.studentList.add(student);
+//        return new ResponseEntity<>(student, HttpStatus.OK);
+//    }
+
+//    @RequestMapping(method = RequestMethod.DELETE)
+//    public RequestEntity<?> delete(@RequestBody Student student) {
+//        Student.studentList.remove(student)
+//        return new ResponseEntity<>(HttpStatus.OK);
+//    }
+//
+//    @RequestMapping(method = RequestMethod.PUT)
+//    public RequestEntity<?> update(@RequestBody Student student) {
+//      Student.studentList.remove(student);
+//       student.studentList.add(student);
+//      return new ResponseEntity<>(HttpStatus.OK);
+//    }
+
+
+
+}
 
 
--- src/main/java/br/com/devdojo/awesome/model/Student.java
+++ src/main/java/br/com/devdojo/awesome/model/Student.java
@@ -29,6 +29,8 @@ public class Student {
     }
 
     public Student() {
+
+
     }
 
    public static void studentRepository(){
