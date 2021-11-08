9dba302050e8e89d4a94f1ff90b8d79f51baefc2
Naim Chagas <re040879@qintess.com>
2021-11-08 13:42:07 -0300

    Aula 12 - Parte 3 - Transacoes | Metodo Post - RollBack

--- src/main/java/br/com/devdojo/awesome/edpoint/StudentEndpoint.java
+++ src/main/java/br/com/devdojo/awesome/edpoint/StudentEndpoint.java
@@ -7,6 +7,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -43,7 +44,7 @@ public class StudentEndpoint {
 	}
 
 	@PostMapping
-	//@Transactional(rollbackFor = Exception.class)
+	@Transactional(rollbackFor = Exception.class)
 	public ResponseEntity<?> save(@Valid @RequestBody Student student) {
 		return new ResponseEntity<>(studentDAO.save(student), HttpStatus.OK);
 	}
